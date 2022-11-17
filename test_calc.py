d3ca4ceeddfaa86638d9f3cad7a44b2f785c48e0
fredtruix <fredricktrust@gmail.com>
2022-11-17 01:15:12 +0100

    add: more test

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,21 @@
 import unittest
 import  calc
+#  python3 -m unittest test_calc.py
 
 class TestCalc(unittest.TestCase):
 
+# all test need to start with the word "test" by convention and with "_" and the function name in this example "add"
     def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result, 15)
+        # we are using the assertEqual tool from unittest
+        self.assertEqual(calc.add(10,5), 15)
+        self.assertEqual(calc.add(-1,1), 0)
+        self.assertEqual(calc.add(-1,-1), -2)
 
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10,5), 5)
+        self.assertEqual(calc.subtract(-1,1), -2)
+        self.assertEqual(calc.subtract(-1,-1), 0)
 
 if __name__ == '__main__':
     unittest.main()
