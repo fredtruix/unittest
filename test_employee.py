6ab4b2c23c04db24fd68f1b56186cf77b6a92dd1
fredtruix <fredricktrust@gmail.com>
2022-11-17 08:44:21 +0100

    add test for fullname

--- test_employee.py
+++ test_employee.py
@@ -6,11 +6,11 @@ class TestEmpoyee(unittest.TestCase):
 
     def test_email(self):
         emp_1 = Employee("fredrick","trust","50000")
-        emp_2 = Employee("John","doe","100000")
+        emp_2 = Employee("john","doe","100000")
 
 
         self.assertEqual(emp_1.email, "fredricktrust@gmail.com")
-        self.assertEqual(emp_2.email, "Johndoe@gmail.com")
+        self.assertEqual(emp_2.email, "johndoe@gmail.com")
 
         emp_1.first = "orume"
         emp_2.first = "berry"
@@ -20,6 +20,19 @@ class TestEmpoyee(unittest.TestCase):
         self.assertEqual(emp_2.email, "berrydoe@gmail.com")
 
 
+    def test_fullname(self):
+        emp_1 = Employee("fredrick","trust","50000")
+        emp_2 = Employee("john","doe","100000")
+
+        self.assertEqual(emp_1.fullname, "fredrick trust")
+        self.assertEqual(emp_2.fullname, "john doe")
+
+
+        emp_1.first = "orume"
+        emp_2.first = "berry"
+
+        self.assertEqual(emp_1.fullname, "orume trust")
+        self.assertEqual(emp_2.fullname, "berry doe")
 
 
 
